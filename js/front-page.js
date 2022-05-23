0c4110837dd60bbb54bd616cf3ad34d0183acccb
petermei82 <105027136+petermei82@users.noreply.github.com>
2022-05-23 17:04:56 +0800

    首頁-step-1-ver.1

---
+++ js/front-page.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    mousewheel: true,
+    centeredSlides: true,
+    spaceBetween: 75,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
