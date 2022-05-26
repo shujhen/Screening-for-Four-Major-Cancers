639a0309f9dd9a2443e67236d4a6710ef39e06f1
yunian12 <105027296+yunian12@users.noreply.github.com>
2022-05-26 14:54:21 +0800

    介紹-卡片翻面刪除

--- js/introduce.js
+++ js/introduce.js
@@ -1,87 +1,73 @@
-// 翻背面
-let but1 = document.querySelectorAll('.button1');
-let but2 = document.querySelectorAll('.button2');
-let but3 = document.querySelectorAll('.button3');
-let but4 = document.querySelectorAll('.button4');
-let card1 = document.querySelector('.card1');
-let card2 = document.querySelector('.card2');
-let card3 = document.querySelector('.card3');
-let card4 = document.querySelector('.card4');
-let card_back1 = document.querySelector('.card-back1');
-let card_back2 = document.querySelector('.card-back2');
-let card_back3 = document.querySelector('.card-back3');
-let card_back4 = document.querySelector('.card-back4');
+function changeText1() {
+    let element = document.getElementById("btn1");
+    // 按鈕改變
+    if (element.value == "口腔癌介紹")
+        element.value = "口腔癌篩檢介紹";
+    else
+        element.value = "口腔癌介紹";
+    // 文字改變
+    let btn1_p1 = document.querySelector('.btn1_p1');
+    let btn1_p2 = document.querySelector('.btn1_p2');
+    if (element.value == "口腔癌介紹") {
+        btn1_p1.innerHTML = "口腔黏膜檢查是由醫生目視或觸診口腔黏膜，看有沒有疑似癌前病變或癌症的病兆，不會疼痛，也沒有副作用。"
+        btn1_p2.innerHTML = "檢查結果若為陰性，請持續每2年接受1次口腔黏膜檢查。若為陽性，請根據醫生指示做後續治療。"
+    } else if (element.value = "口腔癌篩檢介紹") {
+        btn1_p1.innerHTML = "口腔癌為我國青壯年（25-44歲）男性最容易發生的癌症，平均死亡年齡為54歲，較其他癌症早10年以上。"
+        btn1_p2.innerHTML = "每年約有5,400名口腔癌個案，2,300人死於口腔癌，嚼檳榔、吸菸是造成口腔癌的主因。罹病後，輕則毀容，重則奪命。篩檢政策 國外研究顯示，口腔黏膜檢查可有效降低口腔癌之死亡率。"
+    }
+}
 
-but1.forEach((e) => {
-    console.log(e);
-    e.addEventListener('click', function () {
-        card1.classList.add('cardk')
-        card_back1.classList.add('cardS')
-        card_back1.classList.add('card-backNew')
-        card_back1.style.marginTop = '0 0 0 0!important'
-    })
-})
-
-but2.forEach((e) => {
-    console.log(e);
-    e.addEventListener('click', function () {
-        card2.classList.add('cardk')
-        card_back2.classList.add('cardS')
-        card_back2.classList.add('card-backNew')
-        card_back2.style.marginTop = '0.5rem 0 0 0!important'
-    })
-})
-
-but3.forEach((e) => {
-    console.log(e);
-    e.addEventListener('click', function () {
-        card3.classList.add('cardk')
-        card_back3.classList.add('cardS')
-        card_back3.classList.add('card-backNew')
-        card_back3.style.marginTop = '0.5rem 0 0 0!important'
-    })
-})
-
-but4.forEach((e) => {
-    console.log(e);
-    e.addEventListener('click', function () {
-        card4.classList.add('cardk')
-        card_back4.classList.add('cardS')
-        card_back4.classList.add('card-backNew')
-        card_back4.style.marginTop = '0.5rem 0 0 0!important'
-    })
-})
-
-// 翻回正面
-let back1 = document.querySelector('.back');
-let back2 = document.querySelector('.back2');
-let back3 = document.querySelector('.back3');
-let back4 = document.querySelector('.back4');
-
-back1.addEventListener('click', function(){
-    card1.classList.remove('cardk')
-    card_back1.classList.remove('cardS')
-    card_back1.classList.remove('card-backNew')
-    card_back1.style.marginTop = '0.5rem 0 0 0!important'
-});
-
-back2.addEventListener('click', function(){
-    card2.classList.remove('cardk')
-    card_back2.classList.remove('cardS')
-    card_back2.classList.remove('card-backNew')
-    card_back2.style.marginTop = '0.5rem 0 0 0!important'
-});
-
-back3.addEventListener('click', function(){
-    card3.classList.remove('cardk')
-    card_back3.classList.remove('cardS')
-    card_back3.classList.remove('card-backNew')
-    card_back3.style.marginTop = '0.5rem 0 0 0!important'
-});
-
-back4.addEventListener('click', function(){
-    card4.classList.remove('cardk')
-    card_back4.classList.remove('cardS')
-    card_back4.classList.remove('card-backNew')
-    card_back4.style.marginTop = '0.5rem 0 0 0!important'
-});
+function changeText2() {
+    let element = document.getElementById("btn2");
+    // 按鈕改變
+    if (element.value == "乳癌介紹")
+        element.value = "乳癌篩檢介紹";
+    else
+        element.value = "乳癌介紹";
+    // 文字改變
+    let btn2_p1 = document.querySelector('.btn2_p1');
+    let btn2_p2 = document.querySelector('.btn2_p2');
+    if (element.value == "乳癌介紹") {
+        btn2_p1.innerHTML = "乳房X光攝影檢查過程中，女性放射師會以壓迫板緊壓乳房， 以獲得清晰的影像並減少輻射劑量，所以會有疼痛感。"
+        btn2_p2.innerHTML = "檢查結果若無異常，請持續每2年接受1次乳房X光攝影檢查。若有無法確定診斷之變化，需再做其他影像檢查 。若有疑似良性變化，請3-6個月進行追蹤內檢查。吸菸是造成口腔癌的主因。罹病後，輕則毀容，重則奪命。篩檢政策 國外研究顯示，口腔黏膜檢查可有效降低口腔癌之死亡率。"
+    } else if (element.value = "乳癌篩檢介紹") {
+        btn2_p1.innerHTML = "乳癌為我國婦女發生率第1位之癌症，發生高峰約在45-69歲之間，約每十萬名婦女188-194人。"
+        btn2_p2.innerHTML = "依據衛生福利部死因統計及國民健康署癌症登記資料顯示，每年有逾萬位婦女罹患乳癌，逾2,000名婦女死於乳癌，相當於每天約31位婦女被診斷罹患乳癌、6位婦女因乳癌而失去寶貴性命。定期乳房X光攝影篩檢是最重要的防治策略。"
+    }
+}
+function changeText3() {
+    let element = document.getElementById("btn3");
+    // 按鈕改變
+    if (element.value == "大腸癌介紹")
+        element.value = "大腸癌篩檢介紹";
+    else
+        element.value = "大腸癌介紹";
+    // 文字改變
+    let btn3_p1 = document.querySelector('.btn3_p1');
+    let btn3_p2 = document.querySelector('.btn3_p2');
+    if (element.value == "大腸癌介紹") {
+        btn3_p1.innerHTML = "大腸內的腫瘤或息肉，受到糞便摩擦而出血，血液因而附在糞便表面排出，經採集表面糞便送至檢驗室檢驗，即可測出是否含血液。為避免糞便中血紅素受熱破壞，檢體應放在含保存液之採便管冷藏保存，並儘速送回檢驗。"
+        btn3_p2.innerHTML = "檢查結果若為陰性，請持續每2年接受1次糞便潛血檢查。檢查結果若為陽性，請根據醫生指示做後續治療。"
+    } else if (element.value = "乳癌篩檢介紹") {
+        btn3_p1.innerHTML = "子宮頸抹片檢查會先以擴陰器擴張陰道，再以小木棒或刷子輕輕刮取子宮頸 剝落的細胞，塗抹於玻片和染色後，經顯微鏡觀察是否有 癌前病變或是癌症。"
+        btn3_p2.innerHTML = "檢查結果若正常，請持續每3年至少接受1次子宮頸每抹片檢查。若有意義不明的鱗狀細胞變化 、有輕度癌前病變 、重度癌前病變、意義未明的腺體細胞變化或癌症，請根據醫生指示做後續治療。"
+    }
+}
+function changeText4() {
+    let element = document.getElementById("btn4");
+    // 按鈕改變
+    if (element.value == "乳癌介紹")
+        element.value = "乳癌篩檢介紹";
+    else
+        element.value = "乳癌介紹";
+    // 文字改變
+    let btn4_p1 = document.querySelector('.btn4_p1');
+    let btn4_p2 = document.querySelector('.btn4_p2');
+    if (element.value == "乳癌介紹") {
+        btn4_p1.innerHTML = "子宮頸抹片檢查會先以擴陰器擴張陰道，再以小木棒或刷子輕輕刮取子宮頸 剝落的細胞，塗抹於玻片和染色後，經顯微鏡觀察是否有 癌前病變或是癌症。"
+        btn4_p2.innerHTML = "檢查結果若正常，請持續每3年至少接受1次子宮頸每抹片檢查。若有意義不明的鱗狀細胞變化 、有輕度癌前病變 、重度癌前病變、意義未明的腺體細胞變化或癌症，請根據醫生指示做後續治療。"
+    } else if (element.value = "乳癌篩檢介紹") {
+        btn4_p1.innerHTML = "大腸癌是可以早期發現早期治療，且治癒率很高的癌症。根據統計，早期的大腸癌如果妥善治療，存活率高達90％以上。大腸癌主要是由大腸內的腺瘤瘜肉癌化所造成，因此如能早期發現腺瘤瘜肉予以切除，可以減少大腸癌的發生。"
+        btn4_p2.innerHTML = "透過實施糞便潛血檢查可以降低大腸癌發生率，如果糞便潛血檢查呈陽性時，應進一步接受大腸鏡檢查，其好處是檢查過程如有發現病兆或瘜肉時，可以直接取樣做切片或是將它切除。"
+    }
+}
